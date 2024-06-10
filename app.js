6564ccf840b05f1a903b918dc3fe34b359d4f7b8
atanu16 <atanumax2@gmail.com>
2024-06-10 17:08:55 +0530

    vercel deploy

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
-const https =require("https");
+const http = require("http"); // Use the 'http' module instead of 'https'
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"));
+// Set up static directory
+const publicDir = path.join(__dirname, 'public');
+app.use(express.static(publicDir));
+
+// Middleware for parsing JSON and urlencoded bodies
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-const port = 3000;
 
 // Database connection
 const connection = require('./db.js');
 
 // Routes
-const routes = require('./routes/route')
-app.use(routes)
+const routes = require('./routes/route');
+app.use(routes);
 
 // Server Listen
 connection.then(db => {
-    if(!db) return process.exit(1);
+    if (!db) return process.exit(1);
 
-    // listen to the http server
-    app.listen(port, () => {
-        console.log(`Server is running on port: http://localhost:${port}`)
-    })
+    const PORT = process.env.PORT || 3000;
 
-    app.on('error', err => console.log(`Failed To Connect with HTTP Server : ${err}`));
-    // error in mondb connection
+    // Create HTTP server instead of HTTPS
+    http.createServer(app).listen(PORT, () => {
+        console.log(`Server is running on port: http://localhost:${PORT}`);
+    });
+
+    app.on('error', err => console.log(`Failed To Connect with HTTP Server: ${err}`));
+
+    // Error in MongoDB connection
 }).catch(error => {
     console.log(`Connection Failed...! ${error}`);
 });
